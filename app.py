185ca0d94fd4b8b296dd85953bb2e71342ce4a19
Dane <danesherbs@gmail.com>
2020-01-19 23:04:57 +1100

    Add topic select box

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
-from search import get_academics, get_search_query
-from flask import Flask, request, redirect
+from search import get_academics
+from search import get_search_query
+from search import get_top_level_topics
+
+from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    topic = 'art'
+
     if request.method == 'POST':
         query = request.form['query']
-        academics = get_academics(query)
+        topic = request.form['topic']
+        academics = get_academics(topic)
         search_query = get_search_query(query, academics)
         return redirect(f'https://www.google.com/search?q={search_query}')
 
-    return app.send_static_file('factcheck.html')
+    # fields = get_top_level_topics()
+    # return render_template('index.html', fields=fields)
+
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port= 8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
--- search.py
+++ search.py
@@ -4,53 +4,68 @@ from secrets import MICROSOFT_ACADEMIC_API_KEY
 
 def get_search_query(query, academics):
     exact_matches = [f"\"{a}\"" for a in academics]
-    return query + " AND " + " OR ".join(exact_matches)
+    return f"{query} AND ({' OR '.join(exact_matches)})"
 
 
-def get_academics(query, count=10):
-    calchistogram_url = "https://api.labs.cognitive.microsoft.com/academic/v1.0/calchistogram?"
+def get_top_level_topics(count=100):
+    """
+    e.g. get_top_level_topics()
+    """
 
     params = {
-        "expr": _interpret_query(query),
-        "attributes": "AA.DAuN",
+        "expr": "FL=0",
+        "attributes": "DFN,CC",
+        "count": count,
+        "orderby": "CC:desc"
+    }
+
+    payload = query_knowledge_graph(params, 'evaluate')
+
+    topics = [topic["DFN"] for topic in payload["entities"]]
+
+    return sorted(topics)
+
+
+def get_child_topics(topic, count=500):
+    """
+    e.g. get_child_topics('biochemistry')
+    """
+
+    params = {
+        "expr": f"FN=='{topic}'",
+        "attributes": "FC.FN",
         "count": count,
     }
 
-    headers = {
-        "Ocp-Apim-Subscription-Key": MICROSOFT_ACADEMIC_API_KEY,
-    }
+    payload = query_knowledge_graph(params, 'evaluate')
 
-    response = requests.get(
-            calchistogram_url,
-            params=params,
-            headers=headers,
-    )
+    topics = [topic["FN"] for topic in payload["entities"][0]["FC"]]
 
-    response.raise_for_status()
-    payload = response.json()
-
-    return [item["value"] for item in payload["histograms"][0]["histogram"]]
+    return sorted(topics)
 
 
-def _interpret_query(query):
-    interpret_url = "https://api.labs.cognitive.microsoft.com/academic/v1.0/interpret?"
+def get_academics(topic):
+    """
+    e.g. get_academics('restricted boltzmann machine')
+
+    TODO: figure out how ranking works for calchistogram.
+        Can we sort by number of citations?
+    """
 
     params = {
-        "query": query,
-        "count": 1,
+        "expr": f"Composite(F.FN='{topic}')",
+        "attributes": "AA.AuN",
+        "count": 10,
     }
 
-    headers = {
-        "Ocp-Apim-Subscription-Key": MICROSOFT_ACADEMIC_API_KEY,
-    }
+    payload = query_knowledge_graph(params, 'calchistogram')
 
-    response = requests.get(
-            interpret_url,
-            params=params,
-            headers=headers,
-        )
+    return [author["value"] for author in payload["histograms"][0]["histogram"]]
 
+
+def query_knowledge_graph(params, service):
+    url = f"https://api.labs.cognitive.microsoft.com/academic/v1.0/{service}?"
+    headers = {"Ocp-Apim-Subscription-Key": MICROSOFT_ACADEMIC_API_KEY}
+    response = requests.get(url, params=params, headers=headers)
     response.raise_for_status()
-    payload = response.json()
-
-    return payload["interpretations"][0]["rules"][0]["output"]["value"]
+    return response.json()
