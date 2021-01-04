54a408a283b5865c8352d940f695b088059b30d3
Dane Sherburn <danesherbs@gmail.com>
2021-01-04 16:54:05 +1100

    Update port from 8000 to 80

--- app.py
+++ app.py
@@ -85,4 +85,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=os.environ["ENV"] != "prod")
+    app.run(host="0.0.0.0", port=80, debug=os.environ["ENV"] != "prod")
