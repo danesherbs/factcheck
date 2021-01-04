405db6eb5869cd604bb62b1815836462179e8260
Dane Sherburn <danesherbs@gmail.com>
2021-01-04 16:29:24 +1100

    Update API key to be env variable

--- app.py
+++ app.py
@@ -8,79 +8,79 @@ from flask import Flask, request, redirect, render_template
 app = Flask(__name__)
 
 topics = {
-    'high': get_top_level_topics(),
-    'medium': [],
-    'low': [],
+    "high": get_top_level_topics(),
+    "medium": [],
+    "low": [],
 }
 
 selected = {
-    'high': 'art',
+    "high": "art",
 }
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     global topics
     global selected
 
-    if request.method == 'POST' and 'subsubtopic' in request.form:
-        subsubtopic = request.form['subsubtopic']
+    if request.method == "POST" and "subsubtopic" in request.form:
+        subsubtopic = request.form["subsubtopic"]
 
         topics = {
-            'high': topics['high'],
-            'medium': topics['medium'],
-            'low': topics['low'],
+            "high": topics["high"],
+            "medium": topics["medium"],
+            "low": topics["low"],
         }
 
         selected = {
-            'high': selected['high'],
-            'medium': selected['medium'],
-            'low': subsubtopic,
+            "high": selected["high"],
+            "medium": selected["medium"],
+            "low": subsubtopic,
         }
 
-    elif request.method == 'POST' and 'subtopic' in request.form:
-        subtopic = request.form['subtopic']
+    elif request.method == "POST" and "subtopic" in request.form:
+        subtopic = request.form["subtopic"]
 
         topics = {
-            'high': topics['high'],
-            'medium': topics['medium'],
-            'low': get_child_topics(subtopic),
+            "high": topics["high"],
+            "medium": topics["medium"],
+            "low": get_child_topics(subtopic),
         }
 
         selected = {
-            'high': selected['high'],
-            'medium': subtopic,
+            "high": selected["high"],
+            "medium": subtopic,
         }
 
-    elif request.method == 'POST' and 'topic' in request.form:
-        topic = request.form['topic']
+    elif request.method == "POST" and "topic" in request.form:
+        topic = request.form["topic"]
 
         topics = {
-            'high': topics['high'],
-            'medium': get_child_topics(topic),
-            'low': [],
+            "high": topics["high"],
+            "medium": get_child_topics(topic),
+            "low": [],
         }
 
         selected = {
-            'high': topic,
+            "high": topic,
         }
 
-    elif request.method == 'POST' and 'query' in request.form:
-        query = request.form['query']
+    elif request.method == "POST" and "query" in request.form:
+        query = request.form["query"]
 
-        if 'low' in selected:
-            topic = selected['low']
-        elif 'medium' in selected:
-            topic = selected['medium']
+        if "low" in selected:
+            topic = selected["low"]
+        elif "medium" in selected:
+            topic = selected["medium"]
         else:
-            topic = selected['high']
+            topic = selected["high"]
 
         academics = get_academics(topic)
         search_query = get_search_query(query, academics)
-        return redirect(f'https://www.google.com/search?q={search_query}')
+        return redirect(f"https://www.google.com/search?q={search_query}")
 
-    return render_template('index.html', topics=topics, selected=selected)
+    return render_template("index.html", topics=topics, selected=selected)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
--- search.py
+++ search.py
@@ -1,10 +1,10 @@
+import os
 import requests
 from functools import lru_cache
-from secrets import MICROSOFT_ACADEMIC_API_KEY
 
 
 def get_search_query(query, academics):
-    exact_matches = [f"\"{a}\"" for a in academics]
+    exact_matches = [f'"{a}"' for a in academics]
     return f"{query} AND ({' OR '.join(exact_matches)})"
 
 
@@ -17,10 +17,10 @@ def get_top_level_topics(count=100):
         "expr": "FL=0",
         "attributes": "DFN,CC",
         "count": count,
-        "orderby": "CC:desc"
+        "orderby": "CC:desc",
     }
 
-    payload = query_knowledge_graph(params, 'evaluate')
+    payload = query_knowledge_graph(params, "evaluate")
 
     topics = [topic["DFN"] for topic in payload["entities"]]
 
@@ -41,7 +41,7 @@ def get_child_topics(topic, count=500):
         "count": count,
     }
 
-    payload = query_knowledge_graph(params, 'evaluate')
+    payload = query_knowledge_graph(params, "evaluate")
 
     topics = [topic["FN"] for topic in payload["entities"][0]["FC"]]
 
@@ -64,14 +64,14 @@ def get_academics(topic):
         "count": 10,
     }
 
-    payload = query_knowledge_graph(params, 'calchistogram')
+    payload = query_knowledge_graph(params, "calchistogram")
 
     return [author["value"] for author in payload["histograms"][0]["histogram"]]
 
 
 def query_knowledge_graph(params, service):
     url = f"https://api.labs.cognitive.microsoft.com/academic/v1.0/{service}?"
-    headers = {"Ocp-Apim-Subscription-Key": MICROSOFT_ACADEMIC_API_KEY}
+    headers = {"Ocp-Apim-Subscription-Key": os.environ["MICROSOFT_ACADEMIC_API_KEY"]}
     response = requests.get(url, params=params, headers=headers)
     response.raise_for_status()
     return response.json()
